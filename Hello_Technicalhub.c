869ba3eb0a3fe50657e50c0e1e709cb49a2187a8
Saanika1810 <97437509+Saanika1810@users.noreply.github.com>
2022-03-01 00:27:04 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     char s[100];     //gets(s);     scanf("%[^ ]s",&s);     printf("Hello Technicalhub ");     printf("%s",s);     return 0; }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char s[100];
+    //gets(s);
+    scanf("%[^
+]s",&s);
+    printf("Hello Technicalhub
+");
+    printf("%s",s);
+    return 0;
+}
